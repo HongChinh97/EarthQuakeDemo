e3fb2328429a5b90e8058c3c886a933d02539b53
Hồng Chinh <hongchinh@admins-imac-3.local>
2019-05-22 14:58:12 +0700

    Initial Commit

---
+++ EarthQuakeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EarthQuakeDemo
+//
+//  Created by Hồng Chinh on 5/22/19.
+//  Copyright © 2019 gh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
